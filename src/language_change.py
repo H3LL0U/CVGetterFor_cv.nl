de2a9fad942707d3b7936199b32ded62a57e7646
Konstantin <turbo.foxy01@gmail.com>
2024-06-03 16:12:34 +0200

    Custom name feature added

--- src/language_change.py
+++ src/language_change.py
@@ -1,16 +1,22 @@
 change_language = {
-    "Dutch": (
-        "Typ de view link van jouw CV",
-        "Selecteer het pad waar u de CV wilt opslaan"
-    ),
-    "English": (
-        "Type the view link of your CV",
-        "Select the path where you want the image to be stored"
-    ),
-    "Russian": (
-        "Напишите ссылку на ваше резюме здесь",
-        "Напишите путь где вы хотите сохранить свое резюме"
-    )
+    "Dutch": {
+        0:"Typ de view link van jouw CV",
+        1:"Selecteer het pad waar u de CV wilt opslaan",
+        2:"Indienen",
+        3:"Schrijf de naam van het bestand"
+    },
+    "English": {
+        0:"Type the view link of your CV",
+        1:"Select the path where you want the image to be stored",
+        2:"Submit",
+        3:"Enter the file name(s)"
+    },
+    "Russian": {
+        0:"Напишите ссылку на ваше резюме здесь",
+        1:"Напишите путь где вы хотите сохранить свое резюме",
+        2:"Отправить",
+        3:"Напишите имя для файла(ов)"
+    }
 
 }
 
--- src/widgets.py
+++ src/widgets.py
@@ -20,27 +20,29 @@ class LinkEntry(ctk.CTkFrame):
         
 
         self.select_path_to_save_to = SelectPathGrid(self,current_language=current_language)
-        self.submit_button = ctk.CTkButton(self,text="Submit",command=self.submit)
+        self.select_name = NameFileSelector(self,current_language=current_language)
+        self.submit_button = ctk.CTkButton(self,text=language_change.getStringByID(2,selected_language = current_language) ,command=self.submit)
         self.image_label = ctk.CTkLabel(self,image= None, text = "")
         
         self.entry_label.pack()
         self.link_entry.pack(pady = 3)
         self.select_path_to_save_to.pack()
-        
+        self.select_name.pack()
         self.submit_button.pack(pady = 3)
         self.image_label.pack(pady=5)
         self.pack()
         
     def submit_thread(self):
-
+        
         path = self.select_path_to_save_to.get_written_path()
+        path_to_new_image = path+f"\\{self.select_name.getEntryVal()}"
         url = self.link_contents.get()
-        output = technical_functions.createCVImageFromURL(url,path+"\\CV.png")
+        output = technical_functions.createCVImageFromURL(url,path_to_new_image)
         if type(output) == str:
             messagebox.showerror("An error acured when trying to save your CV",output)
         else:
             try:
-                open_image = Image.open(path+"\\CV.png")
+                open_image = Image.open(path_to_new_image)
                 created_image = CTkImage(open_image, size=tuple((i//4 for i in open_image.size)))
                 self.image_label.configure(image = created_image)
             except Exception as e:
@@ -48,10 +50,12 @@ class LinkEntry(ctk.CTkFrame):
             messagebox.showinfo("Success!",f"Your image has been saved to {path}")
     def submit(self):
         
-        if self.current_submit_thread is None or not self.current_submit_thread.is_alive():
+        if self.current_submit_thread is None or not self.current_submit_thread.is_alive() and self.select_name.file_name_entry_var.get():
             self.current_submit_thread = threading.Thread(target=self.submit_thread)
             self.current_submit_thread.daemon = True
             self.current_submit_thread.start()
+        elif not self.select_name.file_name_entry_var.get():
+            messagebox.showinfo("No name entred","Please select a file name")
         else:
             messagebox.showinfo("Work in progress","Image is being saved... Please wait")
             
@@ -85,4 +89,30 @@ class SelectPathGrid(ctk.CTkFrame):
         self.save_path_variable.set(selected_dir)
     def get_written_path(self):
         return self.save_path_variable.get()
+
+class NameFileSelector(ctk.CTkFrame):
+    def __init__(self,master,current_language = "English",*args, **kwargs):
+        super().__init__(master,*args,*kwargs)
+
+        self.explanation_label = ctk.CTkLabel(self,text=language_change.getStringByID(3,current_language))
+
+        self.main_grid = ctk.CTkFrame(self)
         
+        self.file_name_entry_var = ctk.StringVar()
+        self.file_name_entry_var.set("CV")
+        self.file_name_entry = ctk.CTkEntry(self.main_grid,width=150, height=40, textvariable=self.file_name_entry_var)
+
+        self.selection = (
+            ".png",
+            )
+        self.file_extension_selector = ctk.CTkComboBox(self.main_grid,height=30,values=self.selection,command = self.resizeFunc, width=70)
+        self.file_name_entry.grid(column = 0,row= 0, padx = 5)
+        self.file_extension_selector.grid(column=1, row = 0)
+        self.explanation_label.pack()
+        self.main_grid.pack()
+    def resizeFunc(self,*k):
+        newLen = len(self.file_extension_selector.get())
+        self.file_extension_selector.configure(width=newLen+10)
+    def getEntryVal(self)-> str:
+        return self.file_name_entry_var.get() + self.file_extension_selector.get()
+    
